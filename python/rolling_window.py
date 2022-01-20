cf60210984caa195ed0243b1681c7f21c1eee267
juseong <jinju4948@naver.com>
2022-01-20 15:04:59 +0900

    np rolling window

---
+++ python/rolling_window.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.arange(50)
+
+def rolling_window(array, window_size,freq):
+    shape = (array.shape[0] - window_size + 1, window_size)
+    strides = (array.strides[0],) + array.strides
+    rolled = np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
+    return rolled[np.arange(0,shape[0],freq)]
+
+rolling_window(a,5,4)
